0571852a2e39a942685732ce63d8268b2e6eb1f5
deviceai <device.ai@gmail.com>
2020-01-05 18:02:28 +0200

    html to mustache

--- sweater/src/main/java/com/example/sweater/RootController.java
+++ sweater/src/main/java/com/example/sweater/RootController.java
@@ -5,15 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class RootController {
 
     @GetMapping("/")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="This is root") String name,
-            Model model
+            Map<String, Object> model
     ) {
-        model.addAttribute("name", name);
+        model.put("name", name);
         return "root";
     }
 
