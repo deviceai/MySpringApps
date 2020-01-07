33b153da4f0e767916bead3f1cf919e7b16c08fa
deviceai <device.ai@gmail.com>
2020-01-07 22:22:37 +0200

    user registration one to many

--- sweater/src/main/java/com/example/sweater/domain/Message.java
+++ sweater/src/main/java/com/example/sweater/domain/Message.java
@@ -1,9 +1,6 @@
 package com.example.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -13,13 +10,22 @@ public class Message {
     private String text;
     private String tag;
 
-    public Message() {
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 
-    }
 
-    public Message(String text, String tag) {
+
+    public Message() { }
+
+    public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
+    }
+
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "none";
     }
 
     public Integer getId() {
@@ -45,4 +51,8 @@ public class Message {
     public void setTag(String tag) {
         this.tag = tag;
     }
+
+    public User getAuthor() { return author; }
+
+    public void setAuthor(User author) { this.author = author; }
 }
